68ebc21cbaf7aab4a4d80cdf42c3cff0eb2204a7
Theo Pelegrina <71673834+theit0@users.noreply.github.com>
2023-11-08 17:18:30 -0300

    header y footer responsive

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,36 +1,40 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import { useState } from 'react';
 import {useNavigate} from 'react-router-dom'
+import { Link } from 'react-router-dom'
+
+import './Header.css'
 
 const Header = () => {
 
     const navigate = useNavigate();
+    
+    const [isMenuToggled,setIsMenuToggled] = useState(false);
 
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand onClick={()=>navigate('/')}>React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link onClick={()=>navigate('/')}>Home</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar> 
+    <header>
+         <article>
+            <Link to="/">
+              <img src="src/assets/logobsnegro.svg" width={100}/>
+            </Link>
+         </article>  
+         <nav>
+            <div className={isMenuToggled ? 'navlinks-dropped' : 'navlinks'} >
+                <Link to="/" className='navlink'><img src="src/assets/home.svg"/>Inicio</Link>
+            </div>
+            
+            <button className='dropdown-button' onClick={() => setIsMenuToggled(!isMenuToggled)}>
+                {
+                  isMenuToggled &&
+                  <img src='src/assets/menuicon.svg'/>
+                }
+                {
+                  !isMenuToggled &&
+                  <img src='src/assets/x.svg'/>
+                }
+            </button>
+
+         </nav>   
+    </header>
   )
 }
 
